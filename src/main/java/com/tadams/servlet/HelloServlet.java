f76d826cec9b92f453d76b4d5253dce21b9377e4
Taylor Adams <tdadams0@gmail.com>
2019-06-04 15:09:54 -0400

    init

---
+++ src/main/java/com/tadams/servlet/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.tadams.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		res.getWriter().write("Hello from my app.");
+	}
+	
+}
